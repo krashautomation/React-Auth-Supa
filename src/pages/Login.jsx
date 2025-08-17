8e5f434428e8e3d398866f16c253c299c90d3f1b
krashautomation <starcashcrypto@gmail.com>
2025-08-17 12:31:31 -0700

    feat: added login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { supabase } from '../client';
 
-const login = () => {
+
+const Login = () => {
+
+  const [formData,setFormData] = useState({
+    email:'',
+    password:''
+  })
+
+console.log(formData)
+
+
+function handleChange(event){
+   setFormData(prevFormData=>{
+    return{
+      ...prevFormData,
+      [event.target.name]:event.target.value
+    }
+   })
+}
+
+async function handleSubmit(e){
+
+  e.preventDefault()
+ try {
+  
+const { data, error } = await supabase.auth.signInWithPassword({
+  email: formData.email,
+  password: formData.password,
+})
+
+console.log(data)
+// alert('Check your email for verification link')
+
+ } catch (error) {
+  alert(error)
+  
+ }
+
+}
+
   return (
-    <div>login</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+    
+   
+<input placeholder='Email'
+name='email'
+onChange={handleChange}/>
+
+      <input placeholder='Password'
+      name='password'
+      onChange={handleChange} 
+      type='password'
+      minLength='6'/>
+
+      <button type='submit'>
+Submit
+      </button>
+      
+    </form>
+           
+    <p>Need to create an account? <Link to='/signup'>Signup</Link></p>
+    </div>
   )
 }
 
-export default login
+export default Login
