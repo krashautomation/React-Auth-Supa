bef77462b39aa652033c19bdff72b3b6ce728e17
krashautomation <starcashcrypto@gmail.com>
2025-08-17 05:59:57 -0700

    feat: add signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const App = () => {
+
+function handleChange(event){
+   
+}
+
   return (
-    <div>App</div>
+    <div><form>
+      <input placeholder='fullname'
+      name='fullName'
+      onChange={handleChange}/>
+
+<input placeholder='Email'
+name='email'
+onChange={handleChange}/>
+
+      <input placeholder='Password'
+      name='password'
+      onChange={handleChange} />
+      
+    </form>
+    </div>
   )
 }
 
