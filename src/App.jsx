c8b3cba6ecc91896a7e8101815abdef4272825fb
krashautomation <starcashcrypto@gmail.com>
2025-08-17 11:58:52 -0700

    feat: added login signup using Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { SignUp, Login } from './pages'; 
+import { Routes, Route } from "react-router-dom";
+
 
 const App = () => {
-
-  const [formData,setFormData] = useState({
-    fullName:'',
-    email:'',
-    password:''
-  })
-
-console.log(formData)
-
-
-function handleChange(event){
-   setFormData(prevFormData=>{
-    return{
-      ...prevFormData,
-      [event.target.name]:event.target.value
-    }
-   })
-}
-
   return (
-    <div><form>
-      <input placeholder='fullname'
-      name='fullName'
-      onChange={handleChange}/>
-
-<input placeholder='Email'
-name='email'
-onChange={handleChange}/>
-
-      <input placeholder='Password'
-      name='password'
-      onChange={handleChange} />
-
-      <button type='submit'>
-Submit
-      </button>
-      
-    </form>
+    <div>
+      <Routes>
+        <Route path={'/signup'} element={<SignUp />} />
+        <Route path={'/'} element={<Login />} />
+      </Routes>
+            
     </div>
-  )
+     )
 }
 
 export default App
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { supabase } from '../client';
+
+
+const SignUp = () => {
+
+  const [formData,setFormData] = useState({
+    fullName:'',
+    email:'',
+    password:''
+  })
+
+console.log(formData)
+
+
+function handleChange(event){
+   setFormData(prevFormData=>{
+    return{
+      ...prevFormData,
+      [event.target.name]:event.target.value
+    }
+   })
+}
+
+async function handleSubmit(e){
+
+  e.preventDefault()
+ try {
+  
+ const { data, error } = await supabase.auth.signUp(
+  {
+    email: formData.email,
+    password: formData.password,
+    options: {
+      data: {
+        full_name: formData.fullName,
+      }
+    }
+  }
+)
+alert('Check your email for verification link')
+
+ } catch (error) {
+  alert(error)
+  
+ }
+
+}
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+    
+      <input placeholder='Fullname'
+      name='fullName'
+      onChange={handleChange}/>
+
+<input placeholder='Email'
+name='email'
+onChange={handleChange}/>
+
+      <input placeholder='Password'
+      name='password'
+      onChange={handleChange} 
+      type='password'
+      minLength='6'/>
+
+      <button type='submit'>
+Submit
+      </button>
+      
+    </form>
+           
+    <p>Already have an account? <Link to='/'>Login</Link></p>
+    </div>
+  )
+}
+
+export default SignUp
